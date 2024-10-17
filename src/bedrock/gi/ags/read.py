4b0378ca83fa6b57afcf4a4dc164f189d7e4bf2d
Joost Gevaert <jhngevaert@gmail.com>
2024-10-17 20:09:16 +0200

    Fix bug when reading headings that spanned multiple lines and coerce_strings when reading data

--- src/bedrock/gi/ags/read.py
+++ src/bedrock/gi/ags/read.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional
+from typing import Any, Dict, List, Union
 
 import pandas as pd
 
@@ -38,9 +38,13 @@ def ags_to_dfs(ags_data: str) -> Dict[str, pd.DataFrame]:
     is_proj_group_correct = check_ags_proj_group(ags_dfs["PROJ"])
     if is_proj_group_correct:
         project_id = ags_dfs["PROJ"]["PROJ_ID"].iloc[0]
-        print(f"AGS {ags_version} data was read for Project {project_id}")
-        print("This Ground Investigation data contains groups:")
-        print(list(ags_dfs.keys()), "\n")
+        print(
+            f"AGS {ags_version} data was read for Project {project_id}",
+            "This Ground Investigation data contains groups:",
+            list(ags_dfs.keys()),
+            sep="\n",
+            end="\n\n",
+        )
 
     return ags_dfs
 
@@ -58,36 +62,40 @@ def ags3_to_dfs(ags3_data: str) -> Dict[str, pd.DataFrame]:
 
     # Initialize dictionary and variables used in the AGS 3 read loop
     ags3_dfs = {}
+    line_type = "line_0"
     group = ""
     headers: List[str] = ["", "", ""]
-    group_data: List[List[Optional[str]]] = [[], [], []]
+    group_data: List[List[Any]] = [[], [], []]
 
     for i, line in enumerate(ags3_data.splitlines()):
+        last_line_type = line_type
+
         # In AGS 3.1 group names are prefixed with **
         if line.startswith('"**'):
+            line_type = "group_name"
             if group:
                 ags3_dfs[group] = pd.DataFrame(group_data, columns=headers)
 
-            group = line.strip(' "*')
+            group = line.strip(' ,"*')
             group_data = []
 
         # In AGS 3 header names are prefixed with "*
         elif line.startswith('"*'):
+            line_type = "headers"
             new_headers = line.split('","')
-            new_headers = [h.strip(' "*') for h in new_headers]
+            new_headers = [h.strip(' ,"*') for h in new_headers]
 
             # Some groups have so many headers that they span multiple lines.
-            # Therefore we need to check whether the new headers are a continuation of the previous headers.
-            # new_headers[-2] (the second to last header) is used because:
-            #   1. the first columns in AGS 3 tables are mostly foreign keys
-            #   2. the last column in AGS table is often FILE_FSET
-            if new_headers[-2].split("_")[0] == headers[-2].split("_")[0]:
+            # Therefore we need to check whether the new headers are
+            # a continuation of the previous headers from the last line.
+            if line_type == last_line_type:
                 headers = headers + new_headers
             else:
                 headers = new_headers
 
         # Skip lines where group units are defined, these are defined in the AGS 3 data dictionary.
         elif line.startswith('"<UNITS>"'):
+            line_type = "units"
             continue
 
         # The rest of the lines contain:
@@ -95,13 +103,18 @@ def ags3_to_dfs(ags3_data: str) -> Dict[str, pd.DataFrame]:
         # 2. a continuation of the previous line. These lines contain "<CONT>" in the first column.
         # 3. are empty or contain worthless data
         else:
+            line_type = "data_row"
             data_row = line.split('","')
             if len("".join(data_row)) == 0:
                 # print(f"Line {i} is empty. Last Group: {group}")
                 continue
             elif len(data_row) != len(headers):
                 print(
-                    f"\nCAUTION: The number of columns on line {i} doesn't match the number of columns of group {group}!\n"
+                    f"\n🚨 CAUTION: The number of columns on line {i + 1} ({len(data_row)}) doesn't match the number of columns of group {group} ({len(headers)})!",
+                    f"{group} headers: {headers}",
+                    f"Line {i + 1}:      {data_row}",
+                    sep="\n",
+                    end="\n\n",
                 )
                 continue
             # Append continued lines (<CONT>) to the last data_row
@@ -110,24 +123,27 @@ def ags3_to_dfs(ags3_data: str) -> Dict[str, pd.DataFrame]:
                 for j, data in enumerate(data_row):
                     data = data.strip(' "')
                     if data and data != "<CONT>":
-                        # Last data row didn't contain data for this column
                         if last_data_row[j] is None:
-                            last_data_row[j] = data
+                            # Last data row didn't contain data for this column
+                            last_data_row[j] = coerce_string(data)
                         else:
+                            # Last data row already contains data for this column
                             last_data_row[j] = str(last_data_row[j]) + data
             # Lines that are assumed to contain valid data are added to the group data
             else:
                 cleaned_data_row = []
                 for data in data_row:
-                    cleaned_data_row.append(data.strip(' "') or None)
+                    cleaned_data_row.append(coerce_string(data.strip(' "')))
                 group_data.append(cleaned_data_row)
 
     # Also add the last group's df to the dictionary of AGS dfs
-    ags3_dfs[group] = pd.DataFrame(group_data, columns=headers)
+    ags3_dfs[group] = pd.DataFrame(group_data, columns=headers).dropna(
+        axis=1, how="all"
+    )
 
     if not group:
         print(
-            'ERROR: The provided AGS 3 data does not contain any groups, i.e. lines starting with "**'
+            '🚨 ERROR: The provided AGS 3 data does not contain any groups, i.e. lines starting with "**'
         )
 
     return ags3_dfs
@@ -152,3 +168,21 @@ def ags4_to_dfs(ags4_data: str) -> Dict[str, pd.DataFrame]:
     }
 
     return ags4_dfs
+
+
+def coerce_string(string: str) -> Union[None, bool, float, str]:
+    if string.lower() in {"none", "null", ""}:
+        return None
+    elif string.lower() == "true":
+        return True
+    elif string.lower() == "false":
+        return False
+    else:
+        try:
+            value = float(string)
+            if value.is_integer():
+                return int(value)
+            else:
+                return value
+        except ValueError:
+            return string
