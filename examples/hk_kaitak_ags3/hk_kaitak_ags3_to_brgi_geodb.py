45fb83fdfbba64917bb4ec122a4398598d16ef78
Joost Gevaert <jhngevaert@gmail.com>
2025-04-14 14:08:19 +0200

    Update script dependencies and uv.lock

--- examples/hk_kaitak_ags3/hk_kaitak_ags3_to_brgi_geodb.py
+++ examples/hk_kaitak_ags3/hk_kaitak_ags3_to_brgi_geodb.py
@@ -1,7 +1,7 @@
 # /// script
 # requires-python = ">=3.9"
 # dependencies = [
-#     "bedrock-ge==0.1.4",
+#     "bedrock-ge==0.2.0",
 #     "chardet==5.2.0",
 #     "geopandas==1.0.1",
 #     "marimo",
