ecc7ba6fb76e524763054cc83bd9d9b6ac3c7f8e
Joost Gevaert <jhngevaert@gmail.com>
2025-04-14 13:55:50 +0200

    Bump version to 0.2.0 and update descriptions and info in pyproject.toml

--- src/bedrock_ge/__init__.py
+++ src/bedrock_ge/__init__.py
@@ -1,3 +1,3 @@
-"""Bedrock, the open source foundation for ground investigation data"""
+"""Bedrock, the open source foundation for ground engineering."""
 
-__version__ = "0.1.4"
+__version__ = "0.2.0"
