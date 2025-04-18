de5ab7126b8e8fe472ba183040d8b9f39a65f5ba
Joost Gevaert <jhngevaert@gmail.com>
2025-04-18 13:34:01 +0200

    Minor adjustments to the concatenate_databases docstrings

--- src/bedrock_ge/gi/concatenate.py
+++ src/bedrock_ge/gi/concatenate.py
@@ -8,10 +8,12 @@ def concatenate_databases(
     db1: Dict[str, Union[pd.DataFrame, gpd.GeoDataFrame]],
     db2: Dict[str, Union[pd.DataFrame, gpd.GeoDataFrame]],
 ) -> Dict[str, pd.DataFrame]:
-    """Concatenate two dictionaries of pandas DataFrames into one dict of DataFrames.
+    """Concatenate two dictionaries of DataFrames into one dict of DataFrames.
 
-    The function concatenates the pandas DataFrames of the second dict of DataFrames to the first dict of DataFrames for the keys they have in common.
-    Keys that are unique to either dictionary will be included in the final concatenated dictionary.
+    The function concatenates the pandas DataFrames of the second dict of
+    DataFrames to the first dict of DataFrames for the keys they have in common.
+    Keys that are unique to either dictionary will be included in the final
+    concatenated dictionary.
 
     Args:
         db1 (Dict[str, pd.DataFrame]): A dictionary of pandas DataFrames, i.e. a database.
