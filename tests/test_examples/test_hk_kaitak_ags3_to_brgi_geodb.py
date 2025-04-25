5187b4f70eb38493d4a9e8c0f80fc4b7a139550a
Joost Gevaert <jhngevaert@gmail.com>
2025-04-25 23:04:57 +0200

    Make sure that the test doesn't mess up git history every time it runs

--- tests/test_examples/test_hk_kaitak_ags3_to_brgi_geodb.py
+++ tests/test_examples/test_hk_kaitak_ags3_to_brgi_geodb.py
@@ -1,6 +1,9 @@
 import os
+import shutil
 import subprocess
 import sys
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 
 def test_kaitak_ags3_notebook_runs_and_creates_gpkg(examples_dir):
@@ -8,35 +11,50 @@ def test_kaitak_ags3_notebook_runs_and_creates_gpkg(examples_dir):
     notebook_path = notebook_dir / "hk_kaitak_ags3_to_brgi_geodb.py"
     gpkg_output_path = notebook_dir / "kaitak_gi.gpkg"
 
-    # Remove the output GeoPackage if it exists
-    if os.path.exists(gpkg_output_path):
-        os.remove(gpkg_output_path)
-
-    print(f"Running: `python {notebook_path}`\n")
-    # Run the notebook as a script
-    # TODO: implement logging
-    # NOTE: The env (environment variables) and encoding are required for running
-    # the notebook as a script from both Windows and Linux. Wihtout: UnicodeDecodeError
-    env = os.environ.copy()
-    env["PYTHONIOENCODING"] = "utf-8"
-    result = subprocess.run(
-        # ["uvx", "marimo", "run", "--headless", "--sandbox", str(notebook_path)],
-        [sys.executable, str(notebook_path)],
-        check=False,
-        capture_output=True,
-        text=True,
-        encoding="utf-8",
-        env=env,
-    )
-
-    # Check that the script ran successfully
-    assert result.returncode == 0, (
-        f"\n📛 Running `uvx run marimo notebook.py` failed with code {result.returncode}\n"
-        f"📄 STDOUT:\n{result.stdout}\n"
-        f"⚠️ STDERR:\n{result.stderr}"
-    )
-
-    # Check that the file was created
     assert gpkg_output_path.exists(), (
-        f"The expected GeoPackage {gpkg_output_path} was not created."
+        f"Expected {gpkg_output_path} to exist, but it doesn't."
     )
+
+    # Copy the kaitak_gi.gpkg to a temporary directory for comparing
+    # to the one created when executing the notebook.
+    # And to put back to the original state at the end of the test.
+    with TemporaryDirectory() as temp_dir:
+        temp_gpkg_path = Path(temp_dir) / "temp_kaitak_gi.gpkg"
+        shutil.move(gpkg_output_path, temp_gpkg_path)
+
+        print(f"Running: `python {notebook_path}`\n")
+        # Run the notebook as a script
+        # TODO: implement logging
+        # NOTE: The env (environment variables) and encoding are required for running
+        # the notebook as a script from both Windows and Linux. Wihtout: UnicodeDecodeError
+        env = os.environ.copy()
+        env["PYTHONIOENCODING"] = "utf-8"
+        result = subprocess.run(
+            # ["uvx", "marimo", "run", "--headless", "--sandbox", str(notebook_path)],
+            [sys.executable, str(notebook_path)],
+            check=False,
+            capture_output=True,
+            text=True,
+            encoding="utf-8",
+            env=env,
+        )
+
+        # Check that the script ran successfully
+        assert result.returncode == 0, (
+            f"\n📛 Running `uvx run marimo notebook.py` failed with code {result.returncode}\n"
+            f"📄 STDOUT:\n{result.stdout}\n"
+            f"⚠️ STDERR:\n{result.stderr}"
+        )
+
+        # Check that the file was created
+        assert gpkg_output_path.exists(), (
+            f"The expected GeoPackage {gpkg_output_path} was not created."
+        )
+
+        # TODO: write some logic to compare the original and new GeoPackages.
+
+        # Remove the newly generated kaitak_gi.gpkg
+        os.remove(gpkg_output_path)
+        # Place back the original kaitak_gi.gpkg from the temporary directory
+        # to its original location.
+        shutil.move(temp_gpkg_path, gpkg_output_path)
